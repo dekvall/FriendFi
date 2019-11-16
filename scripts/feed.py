0c75a633c38df5b613a986c0db3122fd178edd83
dekvall <dkvldev@gmail.com>
2019-11-16 12:40:19 +0200

    add feed script, macs are masked :(

--- scripts/feed.py
+++ scripts/feed.py
@@ -3,12 +3,13 @@ import time
 import requests
 import json
 import parser
+import sqlite3 as db
 
 REAL_TIME = 'http://13.48.149.61:8000/notify.json'
 MOVING_DATA = 'http://13.48.149.61:8000/data.json'
 HEADERS = {'Content-Type': 'application/json'}
 
-def poll_endpoint():
+def poll_notify():
 	response = requests.get(REAL_TIME, headers=HEADERS)
 	if response.status_code == 200:
 		return json.loads(response.content.decode('utf-8'))
@@ -17,19 +18,29 @@ def poll_endpoint():
 		return None
 
 if __name__ == '__main__':
+	last_timestamp = None
+
+	sqlite_file = 'feed_today.sqlite'
+	conn = db.connect(sqlite_file)
+
 	while True:
 		macs = None
+		data = None
 		try:
-			data = poll_endpoint()
+			data = poll_notify()
 		except json.decoder.JSONDecodeError:
-			print("Decoding failed")
-
+			print("!!!!Decoding failed!!!!")
+			time.sleep(.5)
+			continue
 		df = parser.parse(data)
-		macs = parser.extract_macs(df)
+		timestamp = df.timestamp.unique()[0] #Same timestamp for all
 
-		if '08:C5:E1:1F:E7:C4'.lower() in macs:
-			print("="*5 + "ERIK IS MOVING" + "="*5)
-		if 'D8:BB:2C:D2:E6:C3'.lower() in macs:
-			print("="*5 + "DANNE IS MOVING" + "="*5)
-		print(macs)
-		time.sleep(.2)
+		if timestamp != last_timestamp:
+			rows, cols = df.shape
+			print(f'Writing {rows} record(s) to db:')
+			for index, row in df.iterrows():
+				print(row[1], row[2], row[3], row[10], row[11])
+			df.to_sql('RECORDS', conn, if_exists='append', index=False)
+		last_timestamp = timestamp
+		#Write to dataframe to db maybe, idk
+		time.sleep(1)
