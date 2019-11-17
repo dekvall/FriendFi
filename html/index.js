4514081ba2e5703d92de67074e8a4146213d769e
Erik <erik.von.ke@gmail.com>
2019-11-17 01:49:28 +0100

    bettersave

--- html/index.js
+++ html/index.js
@@ -1,49 +1,11 @@
 
-//var div = document.getElementById('plot');
-var jsondata = 
-[
-    {
-        x:600,
-        y:30
-    },
-    {
-        x:20,
-        y:30
-    },
-    {
-        x:400,
-        y:200
-    },
-    {
-        x:300,
-        y:90
-    },
-    {
-        x:90,
-        y:60
-    },
-    {
-        x:150,
-        y:450
-    },
-    {
-        x:940,
-        y:30
-    },
-    {
-        x:60,
-        y:60
-    }
-
-]
-
 var liveData;
 
-document.getElementById("heatmapContainerWrapper").setAttribute("align", "center");
+
 
 
 var width = 1000
-var height = 700
+var height = 600
 var border = 3
 var bordercolor = "black"
 
@@ -54,7 +16,7 @@ var svg = d3.select("body").append("svg")
     .attr("height", height);
                    
 var myimage = svg.append('image')
-    .attr('xlink:href', 'https://i.imgur.com/LgaU8H8.png')
+    .attr('xlink:href', 'https://i.imgur.com/368Jy2g.jpg')
     .attr('width', width)
     .attr('height', height)
 
@@ -67,96 +29,60 @@ var borderPath = svg.append("rect")
     .style("fill", "none")
     .style("stroke-width", border);
 
-// var circle = svg.append("circle")
-//     .attr("cx", 30)
-//     .attr("cy", 30)
-//     .attr("r", 2)
-//     .style("fill", "red");
 
 
+var circle = svg.append("circle")
+    .attr("class","point")
+    .attr("cx", 10)
+    .attr("cy", 20)
+    .attr("r", 2)
+    .style("fill", "red");
+
+    svg.selectAll("point").remove()
 // function updateData() {
 
-//     // Get the data again
-//     var newX = 200 
-//     var newY = 200
 
-//     // Select the section we want to apply our changes to
-//     var svg = d3.select("body")
+var myVar = setInterval(updateData, 1000);
 
-//     // Make the changes
-//     svg.select("circle")   // change the line
-//         .attr("cx", 100)
-//         .attr("cy", 50)
-//         .attr("r", 2)
-//         .style("fill", "red");  
-
-
-
-var heatmap = h337.create({
-    container: document.getElementById('heatmapContainer'),
-    maxOpacity: 0.5,
-    radius: 50,
-    blur: .90,
-    width:width,
-    height: height,
-    align: "center",
-    // backgroundColor with alpha so you can see through it
-    backgroundColor: 'rgba(255, 255, 255, 0)'
-});
-var heatmapContainer = document.getElementById('heatmapContainerWrapper');
-
-// var nuConfig = {
-//     radius: 10,
-//     maxOpacity: .5,
-//     minOpacity: 0,
-//     blur: .75
-//   };
-
-// heatmapInstance.configure(nuConfig);
-
-for (i = 0; i < jsondata.length; i++) {
-    heatmap.addData({ x: jsondata[i].x, y: jsondata[i].y, value: 1 });
-  }
-
-//heatmap.addData({ x: 100, y: 301, value: 1 });
-//heatmap.addData({ x: 120, y: 301, value: 1 });
-//heatmap.addData({ x: 130, y: 301, value: 1 });
-//heatmap.addData({ x: 140, y: 301, value: 1 });
-//heatmap.addData({ x: 150, y: 301, value: 1 });
-//heatmap.addData({ x: 160, y: 301, value: 1 });
-//heatmap.addData({ x: 170, y: 301, value: 1 });
-//heatmap.addData({ x: 180, y: 301, value: 1 });
-//heatmap.addData({ x: 200, y: 301, value: 1 });
-//heatmap.addData({ x: 210, y: 301, value: 1 });
-//
-//heatmapContainer.onmousemove = heatmapContainer.ontouchmove = function(e) {
-//    // we need preventDefault for the touchmove
-//    e.preventDefault();
-//    var x = e.layerX;
-//    var y = e.layerY;
-//    if (e.touches) {
-//    x = e.touches[0].pageX;
-//    y = e.touches[0].pageY;
-//    console.log(x)
-//    }
-//    
-//    heatmap.addData({ x: x, y: y, value: 1 });
-//};
-//heatmapContainer.onclick = function(e) {
-//    var x = e.layerX;
-//    var y = e.layerY;
-//    heatmap.addData({ x: x, y: y, value: 1 });
-//};
+var theta = -27 * 0.0174532925 // 30 degrees
+//updateData()
+function updateData() {
     
-$.get("/data_feed", function(data){
-    //console.log("hej");
-    console.log(data.length)
-    liveData = JSON.parse(data)
+    
+    $.get("http://130.233.85.210/data_feed", function(data){
+        //console.log("hejhopp");
+        liveData = JSON.parse(data)
 
-    var i = 0;
-    for (i = 0; i < liveData.length; i++) {
-        heatmap.addData({ x: liveData[i].x, y: liveData[i].y, value: 1 });
-    }
-    heatmap.repaint()
-});
+
+        
+        svg.selectAll(".point").remove()
+        var i = 0;
+        for (i = 0; i < liveData.length; i++) {
+            if (liveData[i].floor == 1){
+                // console.log(liveData[i].latitude)
+                // console.log(liveData[i].longitude)
+                // console.log(liveData[i].x)
+                // console.log(liveData[i].y)
+                c = "red"
+                //if (liveData[i].tracked == "1"){
+                //    c = "blue"
+                //}
+                var circle = svg.append("circle")
+                    .attr("class","point")
+                    .attr("cx", (liveData[i].x * Math.cos(theta) + liveData[i].y * Math.sin(theta))*2 - 10)
+                    .attr("cy", (-liveData[i].x * Math.sin(theta) + liveData[i].y * Math.cos(theta))*1-55)
+                    .attr("r", 2)
+                    .style("fill", c);
+            }
+        }
+        
+    });
+
+    //$.get("http://127.0.0.1:8080", function(data){
+    //    console.log(data)
+    //});
+}
+
+
+
     
